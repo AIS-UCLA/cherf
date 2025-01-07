a67b7ea507055f9e59fe79b45ad67b346db2c7b7
Christopher Milan <chrismilan@ucla.edu>
2025-01-07 14:03:54 -0800

    holepunching works

--- app/Client.hs
+++ app/Client.hs
@@ -1,16 +1,19 @@
 module Client where
 
 import qualified Control.Exception as E
-import Control.Monad ((>=>))
+import Control.Monad (liftM2, void, (>=>))
 import Data.Binary (decode, encode)
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Base64 as B64
 import qualified Data.ByteString.Char8 as C8
 import Data.ByteString.Lazy (fromStrict)
 import qualified Data.List.NonEmpty as NE
 import Data.X509.CertificateStore (readCertificateStore)
 import Network.Socket
+import Network.Socket.ByteString (recv, send)
 import Network.TLS
 import Packet
+import System.Environment (getEnv)
 
 client :: [String] -> IO ()
 client ("punch" : t) = punch t
@@ -25,24 +28,39 @@ resolve host port =
 punch :: [String] -> IO ()
 punch [host, port, remote] = withSocketsDo $ do
   addr <- resolve host port
-  E.bracket (open addr) close (doHandshake host port >=> handleConn remote)
+  configDir <- liftM2 (++) (getEnv "HOME") (pure "/.cherf/")
+  fp <- B.readFile $ configDir ++ remote ++ ".sha1"
+  E.bracket (open addr) close (\sock -> doHandshake host port sock >>= handlePunch (ConnectRequest fp) sock)
   where
     open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
+      setSockOptValue sock Linger $ SockOptValue (StructLinger {sl_onoff = 1, sl_linger = 0})
       connect sock $ addrAddress addr
       return sock
 punch _ = putStrLn "usage: cherf client punch <addr> <port> <remote>"
 
+advertise :: [String] -> IO ()
+advertise [host, port] = withSocketsDo $ do
+  addr <- resolve host port
+  E.bracket (open addr) close (\sock -> doHandshake host port sock >>= handlePunch ListenRequest sock)
+  where
+    open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
+      setSockOptValue sock Linger $ SockOptValue (StructLinger {sl_onoff = 1, sl_linger = 0})
+      connect sock $ addrAddress addr
+      return sock
+advertise _ = putStrLn "usage: cherf client advertise <addr> <port>"
+
 doHandshake :: HostName -> ServiceName -> Socket -> IO Context
 doHandshake serverName port sock = do
-  Just store <- readCertificateStore "./server.crt"
+  configDir <- liftM2 (++) (getEnv "HOME") (pure "/.cherf/")
+  Just store <- readCertificateStore (configDir ++ "store")
   ctx <-
     contextNew
       sock
-      (defaultParamsClient "cherf.ais-ucla.org" (C8.pack port)) -- FIXME
+      (defaultParamsClient serverName (C8.pack port))
         { clientHooks =
             defaultClientHooks
               { onCertificateRequest = \_ -> do
-                  cred <- credentialLoadX509 "./sullivan.crt" "./sullivan.key"
+                  cred <- credentialLoadX509 (configDir ++ "cert") (configDir ++ "key")
                   case cred of
                     Left _ -> return Nothing
                     Right c -> return (Just c)
@@ -55,31 +73,23 @@ doHandshake serverName port sock = do
   handshake ctx
   return ctx
 
-handleConn :: String -> Context -> IO ()
-handleConn remote ctx = do
-  fp <- B.readFile $ "./" ++ remote ++ ".sha1"
-  sendData ctx $ encode (ConnectRequest fp)
+handlePunch :: Packet -> Socket -> Context -> IO ()
+handlePunch pkt sock ctx = do
+  sendData ctx $ encode pkt
   pkt <- decode . fromStrict <$> recvData ctx
   case pkt of
-    ConnectData _ -> putStrLn "we got data!"
+    ConnectData addr -> do
+      putStrLn $ "got addr: " ++ show addr
+      bye ctx
+      localAddr <- getSocketName sock
+      close sock
+      sock <- case addr of
+        SockAddrInet {} -> socket AF_INET Stream defaultProtocol
+        SockAddrInet6 {} -> socket AF_INET6 Stream defaultProtocol
+      bind sock localAddr
+      connect sock addr
+      void $ send sock $ C8.pack "hello"
+      tmp <- recv sock 10
+      print tmp
     Error code -> putStrLn $ "error: " ++ show code
-    _ -> putStrLn "unimplemented"
-
-advertise :: [String] -> IO ()
-advertise [host, port] = withSocketsDo $ do
-  addr <- resolve host port
-  E.bracket (open addr) close (doHandshake host port >=> handleAdvertise)
-  where
-    open addr = E.bracketOnError (openSocket addr) close $ \sock -> do
-      connect sock $ addrAddress addr
-      return sock
-advertise _ = putStrLn "usage: cherf client advertise <addr> <port>"
-
-handleAdvertise :: Context -> IO ()
-handleAdvertise ctx = do
-  sendData ctx $ encode ListenRequest
-  pkt <- decode . fromStrict <$> recvData ctx
-  case pkt of
-    ConnectData _ -> putStrLn "got addr"
-    Error code -> putStr $ "error: " ++ show code
     _ -> putStrLn "unimplemented"
