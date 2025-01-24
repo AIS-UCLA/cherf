01b9ea45b66f1a3897d43288ee3a8f82de535675
Christopher Milan <chrismilan@ucla.edu>
2025-01-23 16:37:02 -0800

    fix deadlocks and cert checking

--- app/Server.hs
+++ app/Server.hs
@@ -11,13 +11,13 @@ import Data.ByteString.Lazy (fromStrict)
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Map.Strict as Map
 import qualified Data.X509 as X
+import Data.X509.CertificateStore (makeCertificateStore)
 import Data.X509.Validation (Fingerprint (Fingerprint), getFingerprint)
 import Helpers (logMesg)
 import Network.Socket
 import Network.TLS
 import Packet
 import System.Environment (getEnv)
-import System.X509
 
 newtype ServerState = ServerState (MVar (Map.Map ByteString (SockAddr, MVar SockAddr)))
 
@@ -33,15 +33,10 @@ insertFingerprint (ServerState m) (Fingerprint fp) addr = do
   putMVar m $ Map.insert fp (addr, newM) state
   return newM
 
-deleteFingerprint :: ServerState -> Fingerprint -> IO ()
-deleteFingerprint (ServerState m) (Fingerprint fp) = do
+consumeFingerprint :: ServerState -> Fingerprint -> IO (Maybe (SockAddr, MVar SockAddr))
+consumeFingerprint (ServerState m) (Fingerprint fp) = do
   state <- takeMVar m
   putMVar m $ Map.delete fp state
-
-lookupFingerprint :: ServerState -> Fingerprint -> IO (Maybe (SockAddr, MVar SockAddr))
-lookupFingerprint (ServerState m) (Fingerprint fp) = do
-  state <- takeMVar m
-  putMVar m state
   return (Map.lookup fp state)
 
 showFingerprint :: Fingerprint -> String
@@ -76,7 +71,6 @@ handleConn :: ServerState -> Socket -> SockAddr -> IO ()
 handleConn sem sock peer = do
   configDir <- liftM2 (++) (getEnv "HOME") (pure "/.cherf/")
   Right cred <- credentialLoadX509 (configDir ++ "cert") (configDir ++ "key")
-  store <- getSystemCertificateStore
   ctx <-
     contextNew
       sock
@@ -85,7 +79,7 @@ handleConn sem sock peer = do
           serverShared =
             defaultShared
               { sharedCredentials = Credentials [cred],
-                sharedCAStore = store
+                sharedCAStore = makeCertificateStore [last . (\(X.CertificateChain c) -> c) . fst $ cred]
               },
           serverHooks =
             defaultServerHooks
@@ -98,7 +92,7 @@ handleConn sem sock peer = do
 
 process :: ServerState -> Context -> SockAddr -> Packet -> IO ()
 process sem ctx peer (ConnectRequest fingerprint) = do
-  remote <- lookupFingerprint sem (Fingerprint fingerprint)
+  remote <- consumeFingerprint sem (Fingerprint fingerprint)
   case remote of
     Just (addr, m) -> do
       logMesg $ "connect request from " ++ show peer ++ " to " ++ show addr
@@ -120,7 +114,6 @@ process sem ctx peer ListenRequest = do
       remote <- takeMVar m
       sendData ctx (encode (ConnectData remote))
       bye ctx
-      deleteFingerprint sem fp
     _ -> do
       putStrLn $ "advertising request from " ++ show peer ++ " failed (invalid cert)"
       sendData ctx (encode (Error InvalidCert))
